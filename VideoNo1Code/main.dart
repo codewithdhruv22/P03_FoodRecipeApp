fb05d559cb9c252201dcccd3f6aaa7c3e92d5262
Code With Dhruv <75844991+codewithdhruv22@users.noreply.github.com>
2021-05-31 12:43:45 +0530

    Update main.dart

--- VideoNo1Code/main.dart
+++ VideoNo1Code/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:simple_food_recipe_app/screen/home.dart';
+import 'home.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,10 +12,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-      fontFamily: 'SFPro',
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primarySwatch: Colors.blue,
-
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Home(),
     );
