9383f432707a533c5bc42d3363313304c0b4f8c9
Code With Dhruv <75844991+codewithdhruv22@users.noreply.github.com>
2021-06-02 10:15:09 +0530

    Code Of Video No.3

---
+++ VideoNo3Code/lib/home.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController searchController = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                    Color(0xff213A50),
+                  Color(0xff071938)
+                ]
+              ),
+            ),
+          ),
+          Column(
+            children: [
+
+
+              //Search Bar
+              SafeArea(
+                child: Container(
+                  //Search Wala Container
+
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(24)),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          if((searchController.text).replaceAll(" ", "") == "")
+                          {
+                            print("Blank search");
+                          }else{
+                            Navigator.pushReplacementNamed(context, "/loading",arguments: {
+                              "searchText" : searchController.text,
+                            });
+                          }
+
+                        },
+                        child: Container(
+                          child: Icon(
+                            Icons.search,
+                            color: Colors.blueAccent,
+                          ),
+                          margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
+                        ),
+                      ),
+                      Expanded(
+                        child: TextField(
+                          controller: searchController,
+                          decoration: InputDecoration(
+                              border: InputBorder.none, hintText: "Let's Cook Something!"),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("WHAT DO YOU WANT TO COOK TODAY?", style: TextStyle(fontSize: 33, color: Colors.white),),
+                    SizedBox(height: 10,),
+                    Text("Let's Cook Something New!", style: TextStyle(fontSize: 20,color: Colors.white),)
+                  ],
+                ),
+              )
+
+
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
